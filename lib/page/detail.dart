db7dd94413d80f51a8d89301709ad4987f87550e
kwan <rudghks7816@gmail.com>
2021-10-16 15:24:40 +0900

    [feature] 당근마켓 클론#10

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,6 +1,8 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:carrot_clone/components/manner_temperature_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class DetaulCotentsView extends StatefulWidget {
   final Map<String, String> data;
@@ -141,13 +143,107 @@ class _DetaulCotentsViewState extends State<DetaulCotentsView> {
     );
   }
 
-  Widget _bodyWidget() {
-    return Column(
-      children: [
-        _makeSliderImages(),
-        _sellerInfo(),
-      ],
+  Widget _line() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
     );
+  }
+
+  Widget _contentDetail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            widget.data["title"]!,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          const Text(
+            "디지털/가전 ・ 22시간전",
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+          SizedBox(height: 15),
+          const Text(
+            "가나다라 마\n바사 아자차카 타파하가나다라 \n마바사 아자차카 타파하가나다라 마바사 \n아자차카 타파하가나다라 마바사 아자차카 타파하가나다라 마\n바사 아자차카 타파하가나다라 \n마바사 아자차카 타파하가나다라 마바사 \n아자차카 타파하가나다라 마바사 아자차카 타파하",
+            style: TextStyle(fontSize: 15, height: 1.5),
+          ),
+          SizedBox(height: 15),
+          const Text(
+            "채팅 3 ・ 괌심 17 ・ 조회 299",
+            style: TextStyle(color: Colors.grey, fontSize: 12),
+          ),
+          SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+
+  Widget _otherCellContents() {
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [
+          Text(
+            "판매자님의 판매 상품",
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "모두보기",
+            style: TextStyle(fontSize: 12),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _bodyWidget() {
+    return CustomScrollView(slivers: [
+      SliverList(
+        delegate: SliverChildListDelegate([
+          _makeSliderImages(),
+          _sellerInfo(),
+          _line(),
+          _contentDetail(),
+          _line(),
+          _otherCellContents(),
+        ]),
+      ),
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        sliver: SliverGrid(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+          ),
+          delegate: SliverChildListDelegate(List.generate(20, (index) {
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(height: 120, color: Colors.grey)),
+                  const Text("상품제목", style: TextStyle(fontSize: 14)),
+                  const Text(
+                    "금액",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+        ),
+      ),
+    ]);
   }
 
   Widget _bottomNavigationBar() {
